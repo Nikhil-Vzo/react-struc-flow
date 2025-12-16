c5c947a9cc2c0a57738d0c4817db219e8b075dea
Nikhil Yadav <nikhilyadav200530@gmail.com>
2025-12-16 19:14:32 +0530

    fuck added

---
+++ src/Div.jsx
@@ -0,0 +1,10 @@
+function Div()
+{
+    return(
+        <>
+        <div style={{color: "red"}} className="div">json in div</div>
+        </>
+    )
+}
+
+export default Div;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import Fuck from './Fuck'
+import Div from './Div'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Fuck />
+    <Div />
   </StrictMode>,
 )
