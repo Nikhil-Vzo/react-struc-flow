a1b54a211ec6064d9864e895770aea7a4b6ad594
Nikhil Yadav <nikhilyadav200530@gmail.com>
2025-12-16 19:09:19 +0530

    fucked up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+return(
+  <>
+   <h1>hello world </h1>
+  <h2>this is nikhil</h2>
+  </>
+ 
+)
+  
+}
+
+
+
+export default App
---
+++ src/Fuck.jsx
@@ -0,0 +1,9 @@
+function Fuck()
+{
+    return(
+        <h1>Im fucked up by myself
+        </h1>
+    )
+}
+
+export default Fuck;
